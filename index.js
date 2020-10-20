9540a40ed74e8691c1e78a74a01d3033b25e4afe
Chloe Steffens <caforonda103@yahoo.com>
2020-10-20 05:48:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(superbowls) {
+  let win = superbowls.find(superbowl => superbowl.result === "W")
+  return win ? win.year : undefined;
 }
